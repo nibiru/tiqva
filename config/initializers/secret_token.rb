32514edc14ef99aa8b0ec332d88f262c6feaa1d2
Ben Reich <ben.reich@hotmail.com>
2012-03-26 17:57:09 +0200

    Initial Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiqva::Application.config.secret_token = '6bceb1bf7705313c17915102cce1303dcf6516ba448caaa916a016678709116c4dc545027233e82cbb288898471760389445beaaad2ac3806460e6970536bb19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tiqva::Application.config.session_store :cookie_store, key: '_tiqva_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tiqva::Application.config.session_store :active_record_store
